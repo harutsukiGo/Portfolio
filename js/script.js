bfcf01568489e1e09d36f2c58416673cb1ec15f4
Andriamihaja <manu.andriamihaja-razafimanjato@etu.umontpellier.fr>
2025-04-21 17:39:02 +0200

    page d'accueil ok

--- js/script.js
+++ js/script.js
@@ -2,3 +2,17 @@ function toggleMenu() {
     const menu = document.querySelector('.navbar');
     menu.classList.toggle('visible');
 }
+
+
+function date() {
+    const date = document.getElementById('divFooter');
+    let dateActuel = new Date();
+    date.textContent = ` © ${dateActuel.toLocaleDateString('fr-FR', {
+        year: 'numeric',
+    })} Manu Andriamihaja`;
+ }
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    date();
+})
