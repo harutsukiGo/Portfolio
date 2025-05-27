0a42f9cc843eb5388b5daf34027ed361267dfc1f
Andriamihaja <manu.andriamihaja-razafimanjato@etu.umontpellier.fr>
2025-05-27 21:52:40 +0200

    Modif du fond + modif apprentissage

--- js/navbar.js
+++ js/navbar.js
@@ -7,6 +7,7 @@ document.write(`
         </div>
         <div class="navbar">
             <div class="projets"><a href="index.html#buttonPlus">Mes projets</a></div>
+                        <div class="projets"><a href="competences.html">Portfolio d'apprentissage</a></div>
             <div class="aPropos"><a href="aPropos.html">À propos</a></div>
             <div class="contact"><a href="contact.html">Contact</a></div>
             <div class="imageGit">
